db9dae0d116d05939f6d5d01636dec5e784e23ea
spencer <s@stripywalrus.xyz>
2019-10-14 16:59:37 -0400

    mail, almost

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,7 @@ db.app = app
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import models, forms, \
         account, admin, profile, \
