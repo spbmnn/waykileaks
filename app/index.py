6369777c2ceb2a80f9fbc424299ff2635bb2282d
spencer <s@stripywalrus.xyz>
2019-10-15 18:29:16 +0000

    Needs family fun policy.

--- app/index.py
+++ app/index.py
@@ -1,7 +1,6 @@
-from flask import render_template, request, flash, redirect, url_for
-from flask_login import current_user, login_required
-from app import app, db
-from app.models import User, Speaker, Quote
+from flask import render_template, redirect, url_for
+from flask_login import current_user
+from app import app
 
 @app.route('/')
 def index():
